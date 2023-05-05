22b6a9d1c12cd79fb86f717deafa82a7971dc703
fengfan <1583242261@qq.com>
2023-05-05 13:35:56 +0800

    测试1

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char ch;
+    int count = 0;
+
+    fp = fopen("abc.txt", "r");
+
+    if (fp == NULL) {
+        printf("File does not exist.\n");
+        return 0;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        count++;
+    }
+
+    fclose(fp);
+
+    printf("Number of characters: %d\n", count);
+
+    return 0;
+}
